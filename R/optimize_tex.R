1f136fc1344acde60cd2e6730b0e2d229b5713aa
Roman Kyrychenko <kirichenko17roman@gmail.com>
2025-03-16 16:25:42 +0200

    Add `optimize_tex` function to process and optimize TeX files
    Introduced the `optimize_tex` function to reduce TeX file size by removing redundant lines, rounding floats, and handling duplicate entries. The function includes parameters for customization and strives to streamline large TeX files efficiently. Documentation and examples are provided for user guidance.

---
+++ R/optimize_tex.R
@@ -0,0 +1,58 @@
+#' @title Optimize TeX File
+#' @description Optimizes a TeX file by removing unnecessary lines and rounding floats.
+#' @param file A character string specifying the file path to be optimized.
+#' @param reduce_power A parameter to control the reduction of the file size. Default is 0.
+#' @return None. The function is called for its side effects.
+#' @examples
+#' \dontrun{
+#' optimize_tex("path/to/your/file.tex")
+#' }
+#' @export
+optimize_tex <- function(file, reduce_power = 0) {
+  txt <- readr::read_file(file)
+  
+  txt <- stringr::str_replace_all(txt, "\n\r", "")
+  txt <- stringr::str_replace_all(txt, "\n\n", "\n")
+  
+  txt <- round_floats_in_text(txt)
+  
+  lines <- stringr::str_split_1(string = txt, pattern = ";")
+  ulines <- unique(lines)
+  for (ul in ulines) {
+    to_remove <- which(lines == ul)[-1]
+    if (length(to_remove) > 0) {
+      lines <- lines[-to_remove]
+    }
+  }
+  
+  txt <- paste(lines, collapse = ";")
+  
+  readr::write_file(txt, file)
+  
+  lines <- readLines(con = file)
+  lines <- lines[-which(grepl("\\path\\[clip\\]*", lines, perl = F))]
+  lines <- lines[-which(grepl("\\path\\[use as bounding box*", lines, perl = F))]
+  
+  upath <- unique(lines[grepl("\\path\\[", lines, perl = F)])
+  for (up in upath) {
+    to_remove <- which(lines == up)[-1]
+    if (length(to_remove) > 0) {
+      lines <- lines[-to_remove]
+    }
+  }
+  # remove duplicated \definecolor lines (keep only first)
+  lines <- lines[lines != ""]
+  lines <- lines[lines != dplyr::lag(lines, 1)]
+  if (reduce_power != 0) {
+    if (reduce_power < 1) {
+      reduce_power <- ceiling(1 / reduce_power)
+      flt <- rep(c(rep(F, reduce_power), T), length(lines) / reduce_power)[1:length(lines)]
+    } else {
+      flt <- rep(c(F, rep(T, reduce_power)), length(lines) / reduce_power)[1:length(lines)]
+    }
+    rnd <- as.logical(1 - grepl("\\path\\[.*\\] \\(.*\\) circle \\(.*\\);", lines, perl = F) * flt) & as.logical(1 - grepl("\\t\\(.*\\) --", lines, perl = F) * flt)
+    lines <- lines[rnd]
+  }
+  lines <- lines[which(lines == "\\begin{tikzpicture}[x=1pt,y=1pt]"):which(lines == "\\end{tikzpicture}")]
+  writeLines(lines, con = file)
+}
